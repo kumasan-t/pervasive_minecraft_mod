5e475cca37ade0968df35e020f46afc52609010c
randomBEAR <lorenzo.travagliati@gmail.com>
2016-05-14 21:48:36 +0200

    Rewritten the tree with the Midpoint Circle logic

--- src/main/java/com/weathercraft/minecraft/treegen/TemporizedTreeBlock.java
+++ src/main/java/com/weathercraft/minecraft/treegen/TemporizedTreeBlock.java
@@ -28,6 +28,9 @@ public final class TemporizedTreeBlock extends Block {
     private static IBlockState LOG_BLOCK = Blocks.log.getDefaultState();
     private static IBlockState LADDER_BLOCK = Blocks.ladder.getDefaultState();
 
+    private static int TRUNK_SIZE = 2;
+    private static int OFFSET = 9;
+    private static int MAX_CHIOMA_HEIGHT = 7;
     public TemporizedTreeBlock() {
         super(Material.rock);
         GameRegistry.registerBlock(this,"TreeHouse");
@@ -53,18 +56,30 @@ public final class TemporizedTreeBlock extends Block {
     @Override
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ) {
         int currentTreeHeight = world.getBlockState(pos).getValue(AGE);
-        if (currentTreeHeight == 7 || currentTreeHeight == 14 ) {
-                    placeFloor(world,pos,currentTreeHeight);
-                    placeLeaf(world,pos,currentTreeHeight+1);
+
+
+        //Generate the tree's capocchiella
+        if (currentTreeHeight == 12) {
+            placeCapocchiella(world,pos,currentTreeHeight);
         }
-        if (currentTreeHeight == 15)
-           placeBlockCircleFloor(world,pos,currentTreeHeight,14,LOG_BLOCK);
 
 
+        if ( currentTreeHeight % 5 == 0 && currentTreeHeight != 0) {
+            for (int i = 0; i < TRUNK_SIZE+3; i++ ) {
+                placeBlockPreciseCircle(world,pos,currentTreeHeight*2,i,OFFSET,LOG_BLOCK);
+            }
+        }
         for(int i = 0; i <= currentTreeHeight; i++){
-            placeTrunk(world,pos,i*2);
-            placeTrunk(world,pos,i*2 + 1);
+            for (int j = 0; j <= TRUNK_SIZE; j++) {
+                placeBlockPreciseCircle(world,pos,i*2,j,OFFSET,LOG_BLOCK);
+                placeBlockPreciseCircle(world,pos,i*2 + 1,j,OFFSET,LOG_BLOCK);
+            }
+            //Place the ladder on the center of one side of the circle
+            //Two instructions: one for each
+            world.setBlockState(new BlockPos(pos.getX() + OFFSET,pos.getY() + i*2,pos.getZ() + (OFFSET - TRUNK_SIZE -1)),LADDER_BLOCK);
+            world.setBlockState(new BlockPos(pos.getX() + OFFSET,pos.getY() + i*2 + 1,pos.getZ() + (OFFSET - TRUNK_SIZE -1)),LADDER_BLOCK);
         }
+
 
         if(currentTreeHeight < 15) {
             currentTreeHeight++;
@@ -75,64 +90,46 @@ public final class TemporizedTreeBlock extends Block {
         return true;
     }
 
-    private static void placeTrunk(World world, BlockPos pos, int height) {
-
-        world.setBlockState(new BlockPos(pos.getX() +1, pos.getY() + height, pos.getZ()),LOG_BLOCK);
-        world.setBlockState(new BlockPos(pos.getX() +2, pos.getY() + height, pos.getZ()),LOG_BLOCK);
-
-        world.setBlockState(new BlockPos(pos.getX(), pos.getY() + height, pos.getZ()+1),LOG_BLOCK);
-        world.setBlockState(new BlockPos(pos.getX() +1, pos.getY() + height, pos.getZ()+1),LOG_BLOCK);
-        world.setBlockState(new BlockPos(pos.getX() +2, pos.getY() + height, pos.getZ()+1),LOG_BLOCK);
-        world.setBlockState(new BlockPos(pos.getX() +3, pos.getY() + height, pos.getZ()+1),LOG_BLOCK);
-
-        world.setBlockState(new BlockPos(pos.getX(), pos.getY() + height, pos.getZ()+2),LOG_BLOCK);
-        world.setBlockState(new BlockPos(pos.getX() +1, pos.getY() + height, pos.getZ()+2),LOG_BLOCK);
-        world.setBlockState(new BlockPos(pos.getX() +2, pos.getY() + height, pos.getZ()+2),LOG_BLOCK);
-        world.setBlockState(new BlockPos(pos.getX() +3, pos.getY() + height, pos.getZ()+2),LOG_BLOCK);
-
-        world.setBlockState(new BlockPos(pos.getX() +1, pos.getY() + height, pos.getZ()+3),LOG_BLOCK);
-        world.setBlockState(new BlockPos(pos.getX() +2, pos.getY() + height, pos.getZ()+3),LOG_BLOCK);
-
-        world.setBlockState(new BlockPos(pos.getX()+1, pos.getY() + height, pos.getZ()-1), LADDER_BLOCK);
+    private static void placeBlockPreciseCircle(World world, final BlockPos pos, int height, int radius, int offset, IBlockState blockType) {
+        int d = (5 - radius * 4)/4;
+        int x = 0;
+        int z = radius;
+        do {
+            world.setBlockState(new BlockPos(pos.getX() + offset + x, pos.getY() + height, pos.getZ() + offset + z),blockType);
+            world.setBlockState(new BlockPos(pos.getX() + offset + x, pos.getY() + height, pos.getZ() + offset - z),blockType);
+            world.setBlockState(new BlockPos(pos.getX() + offset - x, pos.getY() + height, pos.getZ() + offset + z),blockType);
+            world.setBlockState(new BlockPos(pos.getX() + offset - x, pos.getY() + height, pos.getZ() + offset - z),blockType);
+            world.setBlockState(new BlockPos(pos.getX() + offset + z, pos.getY() + height, pos.getZ() + offset + x),blockType);
+            world.setBlockState(new BlockPos(pos.getX() + offset + z, pos.getY() + height, pos.getZ() + offset - x),blockType);
+            world.setBlockState(new BlockPos(pos.getX() + offset - z, pos.getY() + height, pos.getZ() + offset + x),blockType);
+            world.setBlockState(new BlockPos(pos.getX() + offset - z, pos.getY() + height, pos.getZ() + offset - x),blockType);
+            if (d < 0) {
+                d += 2 * x + 1;
+            } else {
+                d += 2 * (x - z) + 1;
+                z--;
+            }
+            x++;
+        } while (x <= z);
 
     }
 
-    private static void placeFloor(World world, final BlockPos pos, int height) {
-        BlockPos referencePosition = new BlockPos(pos.getX()-1,pos.getY()+height,pos.getZ()-1);
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                if ((i == 0 && j == 0) || (i == 5 && j == 0) || (i == 0 && j == 5) || (i == 5 && j == 5)) {
-                } else {
-                    world.setBlockState(new BlockPos(referencePosition.getX()+i,referencePosition.getY(),referencePosition.getZ()+j),LOG_BLOCK);
-                }
+    private static void placeCapocchiella(World world, BlockPos pos, int currentTreeHeight) {
+        int sphereRadius = 0;
+        boolean halfSphereRechead = false;
+        for (int chiomaHeight = currentTreeHeight * 2; chiomaHeight < MAX_CHIOMA_HEIGHT * 2 + currentTreeHeight * 2; chiomaHeight++) {
+            for (int i = 0; i < sphereRadius; i++) {
+                placeBlockPreciseCircle(world, pos, chiomaHeight, i, OFFSET, Blocks.diamond_block.getDefaultState());
             }
+
+            if (sphereRadius == MAX_CHIOMA_HEIGHT)
+                halfSphereRechead = true;
+
+            if (halfSphereRechead)
+                sphereRadius--;
+            else
+                sphereRadius++;
         }
     }
-
-    private static void placeLeaf(World world, BlockPos pos, int height){
-        BlockPos referencePosition = new BlockPos(pos.getX()-1,pos.getY()+height,pos.getZ()-1);
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                if ((i == 0 && j == 0) || (i == 5 && j == 0) || (i == 0 && j == 5) || (i == 5 && j == 5)) {
-                } else {
-                    world.setBlockState(new BlockPos(referencePosition.getX()+i,referencePosition.getY(),referencePosition.getZ()+j),LEAF_BLOCK);
-                }
-            }
-        }
-    }
-
-    private static void placeBlockCircleFloor(World world, final BlockPos pos, final int height, final int size, IBlockState blockType) {
-        int shift = (size/2) - 2;
-        BlockPos referencePosition = new BlockPos(pos.getX()- shift,pos.getY()+height,pos.getZ()-shift);
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (i+j < shift || (size -1  - j + i  < shift) || (size - 1 - i + j < shift) || (size - i + size - j - 2 < shift)) {
-                } else {
-                    world.setBlockState(new BlockPos(referencePosition.getX()+i,referencePosition.getY(),referencePosition.getZ()+j),blockType);
-                }
-            }
-        }
-    }
-
 
 }
